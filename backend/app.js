c32f1962aed45ce93675495853f110d6a11f99c8
yug-void-dev <yugsharma2421@gmail.com>
2026-01-20 14:00:32 +0530

    signup-route-added

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,27 @@
-import express from 'express'
+import express from "express";
 
 const app = express();
 
-app.get('/', () => {
-    res.send('Login Page')
-})
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", () => {
+  res.send("Login Page");
+});
+
+app.post("/auth/signup", (req, res) => {
+    try {
+        const data = req.body;
+        
+    }
+    catch (err) {
+        res.send(`Error Occured in form submission ${err.message}`)
+    }
+});
 
 
-const port = process.env.PORT
+
+const port = process.env.PORT;
 app.listen(port, () => {
-    console.log(`Server is listening on ${port} number`);
-})
+  console.log(`Server is listening on ${port} number`);
+});
