826e6b79483b14c9e30bc4672c88f6a1c0e09095
VivekCoderze <dayanandgarg967@gmail.com>
2026-01-20 13:59:14 +0530

    connect db

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,18 @@
-import express from 'express'
-
+import express from "express";
+import connectDB from "./src/db/db";
 const app = express();
 
-app.get('/', () => {
-    res.send('Login Page')
-})
+app.get("/", () => {
+  res.send("Login Page");
+});
 
+app.post("/auth/signin", (req, res) => {
+  const { email, password } = req.body;
+});
 
-const port = process.env.PORT
-app.listen(port, () => {
-    console.log(`Server is listening on ${port} number`);
-})
+const port = process.env.PORT || 4000;
+connectDB().then(() => {
+  app.listen(port, () => {
+    console.log(`Server is listening on ${port} number 👌`);
+  });
+});
---
+++ backend/src/db/db.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+const connectDB = async () => {
+    try{
+        const res = await mongoose.connect(process.env.DBURL)
+        console.log("database connected successfully 🔥!!");
+    }
+    catch(error){
+        console.log("Error");
+    }
+}
+export default connectDB
