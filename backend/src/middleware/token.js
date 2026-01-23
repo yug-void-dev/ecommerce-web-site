8e8351a10cb69a78056372d61e6ec41c6b01f4fb
yug-void-dev <yugsharma2421@gmail.com>
2026-01-23 22:44:09 +0530

    update

--- backend/src/middleware/token.js
+++ backend/src/middleware/token.js
@@ -3,10 +3,7 @@ import JsonWebToken from "jsonwebtoken";
 const authMiddleware = (req, res, next) => {
   const token = req.headers.authorization?.split(" ")[1];
   if (!token) {
-<<<<<<< HEAD
-=======
     console.log(token)  
->>>>>>> 9077cdcd834cd35a3c380f73c75bc7a37c8bd589
     res.status(401).json({ message: "Access denied! Please login again" });
     return;
   }
